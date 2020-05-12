ca8e53936ff94bad62eb0da4c583a91a1b214850
ADMIN <dangnghia031089@gmail.com>
2020-05-12 15:46:19 +0700

    chu vi

---
+++ JavaScript/tính chu vi.js
@@ -0,0 +1,8 @@
+let nhapchieucao
+let nhapchieudai
+nhapchieucao = prompt('nhap chieu cao');
+nhapchieudai = prompt('nhap chieu rong');
+let a = parseInt(nhapchieucao);
+let b = parseInt(nhapchieudai);
+let chuvi = a*b;
+document.write("chu vi la: " + chuvi)
