5cfc1adbef184910444863ea310284e3835197ee
ADMIN <dangnghia031089@gmail.com>
2020-05-12 16:46:14 +0700

    chu vi

---
+++ JavaScript/diemtrungbinh.js
@@ -0,0 +1,14 @@
+let dt = prompt('');
+let dh = prompt("");
+let ds = prompt("");
+let a = parseInt(dt);
+let b = parseInt(dh);
+let c = parseInt(ds);
+let tb = (a + b + c)/3;
+console.log(tb);
+/*document.write('dt =' + dt)
+document.write('tb =' + tb)
+document.write('dh =' + dh)
+document.write('ds = ' + ds)*/
+
+let test = document.getElementById("test");
